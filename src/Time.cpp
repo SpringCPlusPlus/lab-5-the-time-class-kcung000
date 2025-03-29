ac9fbae0dcc712b677bd1e92303a94ba75d022a9
kcung000 <62616153+kcung000@users.noreply.github.com>
2025-03-29 20:51:15 +0000

    Lab 6

--- src/Time.cpp
+++ src/Time.cpp
@@ -5,29 +5,51 @@ using namespace std;
 // Overload the + operator
 Time Time::operator+(const Time &other) const
 {   
+    int h=hours+other.hours;
+    int m=minutes+other.minutes;
+    h=(m/60)+h;
+    m=m%60;
+    Time t(h,m);
+    return t;
 }
 
 // Overload the += operator
 Time& Time::operator+=(const Time &other)
 {   
+    int h=hours+other.hours;
+    int m=minutes+other.minutes;
+    hours=(m/60)+h;
+    minutes=m%60;
+    return *this;
 }
 
 // Overload the < operator
 bool Time::operator<(const Time &other) const
 {
+    int t=minutes+(hours*60);
+    int ot=other.minutes+(other.hours*60);
+    return t<ot;
 }
 
 // Overload the > operator
 bool Time::operator>(const Time &other) const
 {
+    int t=minutes+(hours*60);
+    int ot=other.minutes+(other.hours*60);
+    return t>ot;
 }
 
 // Overload the == operator
 bool Time::operator==(const Time &other) const
 {
+    int t=minutes+(hours*60);
+    int ot=other.minutes+(other.hours*60);
+    return (t-ot)==0;
 }
 
 // Overload the friend function operator<<
 ostream &operator<<(ostream &os, const Time &tm)
 {
+    os << tm.getHours() << ":" << tm.getMinutes();
+    return os;
 }
